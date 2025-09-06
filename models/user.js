a7bb1d04a6eeb905d76e652fa359a3bdb227d29e
queeferstarr <queeferstarr@gmail.com>
2025-09-06 11:21:53 -0400

    add spotify field to userSChema

--- models/user.js
+++ models/user.js
@@ -2,7 +2,13 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     discordId: { type: String, required: true, unique: true },
-    lastfm: { type: String }
+    lastfm: { type: String },
+    spotify: {
+        access_token: String,
+        refresh_token: String,
+        expires_in: Number,
+        updated: Date
+    }
 });
 
 module.exports = mongoose.model('User', userSchema);
